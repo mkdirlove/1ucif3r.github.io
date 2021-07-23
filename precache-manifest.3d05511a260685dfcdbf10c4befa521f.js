d641a9224edd10fbc2e7a7a6017aee395876c977
Hritik Kumbhar <61887775+1ucif3r@users.noreply.github.com>
2021-07-23 02:57:32 -0400

    Add Resume and Bug Fix

---
+++ precache-manifest.3d05511a260685dfcdbf10c4befa521f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11980d10f7b2930eb86c6ddc0957b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81551dbf0847d6d00876",
+    "url": "/static/css/main.111b5713.chunk.css"
+  },
+  {
+    "revision": "e1acf6b56dfa0e41b09a",
+    "url": "/static/js/2.4ffcea60.chunk.js"
+  },
+  {
+    "revision": "9d917c6a00320b252e20e96a1f13133b",
+    "url": "/static/js/2.4ffcea60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1526808f25724366b2",
+    "url": "/static/js/3.bb9426a8.chunk.js"
+  },
+  {
+    "revision": "b718d631ef62c6ddcf2f",
+    "url": "/static/js/4.f206581a.chunk.js"
+  },
+  {
+    "revision": "81551dbf0847d6d00876",
+    "url": "/static/js/main.7ff126da.chunk.js"
+  },
+  {
+    "revision": "cad51b7e4da30f1b3d2c",
+    "url": "/static/js/runtime-main.f197b547.js"
+  }
+]);
