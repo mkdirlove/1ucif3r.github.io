d733c0fee2ba0e7a30140e35d765ba304f29b1c0
Hritik Kumbhar <61887775+1ucif3r@users.noreply.github.com>
2021-08-11 15:01:40 +0530

    Delete static directory

--- static/js/3.bb9426a8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonphritikkumbhar=this.webpackJsonphritikkumbhar||[]).push([[3],{140:function(n,t,r){"use strict";r.r(t),r.d(t,"PageNotFound",(function(){return i}));var u=r(0),e=r.n(u),i=function(){return e.a.createElement("div",null,"Page not found...")}}}]);
-//# sourceMappingURL=3.bb9426a8.chunk.js.map
